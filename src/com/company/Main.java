54f49897a8ede178996c0f9b91d150303a1d777b
SergiuCampeanu <sncampeanu@gmail.com>
2021-10-30 20:36:34 +0300

    m

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,18 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        CourseInMemoryRepo courseRepo = new CourseInMemoryRepo();
+        TeacherInMemoryRepo teacherRepo = new TeacherInMemoryRepo();
+        StudentInMemoryRepo studentRepo = new StudentInMemoryRepo();
+
+
+        CourseController courseController = new CourseController(courseRepo);
+        TeacherController teacherController = new TeacherController(teacherRepo);
+        StudentController studentController = new StudentController(studentRepo);
+
+        MainController mainController = new MainController(courseController, studentController, teacherController);
+
+        RegistrationSystem registrationSystem = new RegistrationSystem(mainController);
+        registrationSystem.startApplication();
     }
 }
